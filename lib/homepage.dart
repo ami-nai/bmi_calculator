6024953c55dd99287866ebeb9bb05e21457dd7fb
ami-nai <amishahriar27@gmail.com>
2024-11-07 00:00:51 +0600

    Implemented UI

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -17,135 +17,152 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFA0D683),
       appBar: AppBar(
-        title: const Text('BMI Calculator'), // Add a title for clarity
+        toolbarHeight: 100,
+        backgroundColor: Color(0xFFF72BF78),
+      
+        centerTitle: true,
+        title: const Text('BMI CALCULATOR',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+        ),
+        ), // Add a title for clarity
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0), // Adds padding around the body
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 20), // Adds spacing between elements
-            Row(
-              children: <Widget>[
-                const SizedBox(width: 25),
-                Expanded( // Changed to Expanded to handle overflow better
-                  flex: 3,
-                  child: TextField(
-                    controller: weightController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Weight",
-                      hintText: "Weight",
-                      prefixIcon: Icon(Icons.scale),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const SizedBox(height: 20), // Adds spacing between elements
+              Row(
+                children: <Widget>[
+                  const SizedBox(width: 25),
+                  Expanded( // Changed to Expanded to handle overflow better
+                    flex: 3,
+                    child: TextField(
+                      controller: weightController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Weight",
+                        hintText: "Weight",
+                        prefixIcon: Icon(Icons.scale),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(width: 20), // Adds space between TextField and Dropdown
-                Expanded( // Changed to Expanded
-                  flex: 1,
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    value: _weightUnit,
-                    items: const [
-                      DropdownMenuItem(value: "Kg", child: Text("Kg")),
-                      DropdownMenuItem(value: "Lbs", child: Text("Lbs")),
-                    ],
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _weightUnit = newValue!;
-                      });
-                    },
-                  ),
-                ),
-                const SizedBox(width: 25),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: <Widget>[
-                const SizedBox(width: 25),
-                Expanded( // Changed to Expanded
-                  flex: 3,
-                  child: TextField(
-                    controller: heightController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Height",
-                      hintText: "Height",
-                      prefixIcon: Icon(Icons.height),
+                  const SizedBox(width: 20), // Adds space between TextField and Dropdown
+                  Expanded( // Changed to Expanded
+                    flex: 1,
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: _weightUnit,
+                      items: const [
+                        DropdownMenuItem(value: "Kg", child: Text("Kg")),
+                        DropdownMenuItem(value: "Lbs", child: Text("Lbs")),
+                      ],
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _weightUnit = newValue!;
+                        });
+                      },
                     ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                Expanded( // Changed to Expanded
-                  flex: 1,
-                  child: DropdownButton<String>(
-                    isExpanded: true,
-                    value: _heightUnit,
-                    items: const [
-                      DropdownMenuItem(value: "cm", child: Text("cm")),
-                      DropdownMenuItem(value: "meter", child: Text("meter")),
-                      DropdownMenuItem(value: "feet", child: Text("feet")),
-                    ],
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        _heightUnit = newValue!;
-                      });
-                    },
+                  const SizedBox(width: 25),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: <Widget>[
+                  const SizedBox(width: 25),
+                  Expanded( // Changed to Expanded
+                    flex: 3,
+                    child: TextField(
+                      controller: heightController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Height",
+                        hintText: "Height",
+                        prefixIcon: Icon(Icons.height),
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(width: 25),
-              ],
-            ),
-            const SizedBox(height: 20), // Spacing before button
-            ElevatedButton(
-              onPressed: () {
-                try {
-                  double weight = double.parse(weightController.text);
-                  double height = double.parse(heightController.text);
-
-                  if (_weightUnit == "Lbs") {
-                    weight = weight * 0.453592; // Convert pounds to kilograms
+                  const SizedBox(width: 20),
+                  Expanded( // Changed to Expanded
+                    flex: 1,
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      value: _heightUnit,
+                      items: const [
+                        DropdownMenuItem(value: "cm", child: Text("cm")),
+                        DropdownMenuItem(value: "meter", child: Text("meter")),
+                        DropdownMenuItem(value: "feet", child: Text("feet")),
+                      ],
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _heightUnit = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 25),
+                ],
+              ),
+              const SizedBox(height: 50), // Spacing before button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFF72BF78),
+                        minimumSize: Size(150, 50),
+                        ),
+                onPressed: () {
+                  try {
+                    double weight = double.parse(weightController.text);
+                    double height = double.parse(heightController.text);
+          
+                    if (_weightUnit == "Lbs") {
+                      weight = weight * 0.453592; // Convert pounds to kilograms
+                    }
+                    if (_heightUnit == "cm") {
+                      height = height / 100; // Convert cm to meters
+                    } else if (_heightUnit == "feet") {
+                      height *= 0.3048; // Convert feet to meters
+                    }
+          
+                    double result = weight / (height * height);
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => Result(result: result.toStringAsFixed(2))));
+                  } catch (e) {
+                    // Show an alert if the input is not valid
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text("Input Error"),
+                          content: const Text("Please enter valid numeric values."),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(); // Close the dialog
+                              },
+                              child: const Text("OK"),
+                            ),
+                          ],
+                        );
+                      },
+                    );
                   }
-                  if (_heightUnit == "cm") {
-                    height = height / 100; // Convert cm to meters
-                  } else if (_heightUnit == "feet") {
-                    height *= 0.3048; // Convert feet to meters
-                  }
-
-                  double result = weight / (height * height);
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => Result(result: result.toStringAsFixed(2))));
-                } catch (e) {
-                  // Show an alert if the input is not valid
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text("Input Error"),
-                        content: const Text("Please enter valid numeric values."),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop(); // Close the dialog
-                            },
-                            child: const Text("OK"),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              },
-              child: const Text("Calculate",
-                  style: TextStyle(
-                    color: Colors.white,
-                    backgroundColor: Colors.greenAccent,
-                  )),
-            )
-          ],
+                },
+                child: const Text("Calculate",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/result.dart
+++ lib/result.dart
@@ -7,16 +7,28 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFA0D683),
       appBar: AppBar(
-        title: const Text('BMI Result'), // Add a title for clarity
+        toolbarHeight: 100,
+        backgroundColor: Color(0xFFF72BF78),
+      
+        centerTitle: true,
+        title: const Text('BMI Result',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+        ),
+        ),
+         // Add a title for clarity
       ),
       body: Center(
+        
         child: Container(
+          
           padding: const EdgeInsets.all(20.0), // Add some padding
           child: Text(
             'Your BMI is: $result',
             style: const TextStyle(
-              color: Colors.greenAccent,
               fontSize: 24, // Increase font size for better visibility
               fontWeight: FontWeight.bold, // Make the text bold
             ),
